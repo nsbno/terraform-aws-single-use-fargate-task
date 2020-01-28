02064d192a4d0592a758913dc260932af95102e4
Colin Coleman <colin.coleman@kontrollsmart.com>
2020-01-28 16:28:05 +0100

    change output from arn to id

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "function_name" {
   value = aws_lambda_function.run_single_fargate_task.id
 }
 
-output "lambda_exec_role_arn" {
-  value = aws_iam_role.lambda_exec.arn
+output "lambda_exec_role_id" {
+  value = aws_iam_role.lambda_exec.id
 }
